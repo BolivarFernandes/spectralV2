0bd50c065eb3a1632a0db0463a42d006dcee0739
Liam Kinne <Liam Kinne>
2016-07-26 09:48:04 +1000

    Main Structrure

--- spectralV2.ino
+++ spectralV2.ino
@@ -1,12 +1,53 @@
-#include "classes/MotorControl.h"
+#include "MotorControl.h"
+#include "LightSense.h"
+#include "UltrasonicSense.h"
+//#include "classes/GyroSense.h"
+//#include "classes/EncoderSense.h"
 
-MotorControl ExampleMotor(8, 9);
+MotorControl MotorLeft(5, 4);
+MotorControl MotorRight(7, 6);
+LightSense LightLeft(A8);
+LightSense LightRight(A10);
+UltrasonicSense SonicMain(A9);
+
+enum progState_t {STANDBY, BEGIN, LINEFOLLOW, WATERTOWER, CHEMSPILL, NUM_MODES}; // NUM_MODES is always last.
+
+progState_t robotState = STANDBY; // Default Mode
 
 void setup() {
 	Serial.begin(9600);
-	ExampleMotor.Set(0.5);
 }
 
 void loop() {
-	Serial.println(ExampleMotor.Get());
+	//Gyro.Update();
+	//
+
+	switch(robotState) {
+		case STANDBY: 		Standby();
+		case BEGIN: 		Begin();
+		case LINEFOLLOW: 	LineFollow();
+		case WATERTOWER: 	WaterTower();
+		case CHEMSPILL:	 	ChemSpill();
+		default: 			Standby();
+	}
+}
+
+void Standby() {
+
+}
+
+void Begin() {
+
+}
+
+void LineFollow() {
+
+}
+
+void WaterTower() {
+
+}
+
+void ChemSpill() {
+
 }
