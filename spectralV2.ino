bfdb327770240f01df085a9732bfa57923c28a0f
Liam Kinne <Liam Kinne>
2016-07-26 10:16:02 +1000

    WaterTower structure

--- spectralV2.ino
+++ spectralV2.ino
@@ -6,13 +6,24 @@
 
 MotorControl MotorLeft(5, 4);
 MotorControl MotorRight(7, 6);
+MotorControl MotorLift(8, 9);
+MotorControl MotorGrab(10, 11);
+MotorControl MotorRotate(2, 3);
 LightSense LightLeft(A8);
 LightSense LightRight(A10);
 UltrasonicSense SonicMain(A9);
 
 enum progState_t {STANDBY, BEGIN, LINEFOLLOW, WATERTOWER, CHEMSPILL, NUM_MODES}; // NUM_MODES is always last.
+enum taskState_t {START, RUNNING, FINISHED};
 
 progState_t robotState = STANDBY; // Default Mode
+
+taskState_t WaterTowerState = START;
+taskState_t ChemSpillState = START;
+
+// Time
+long initTime;
+long currTime;
 
 void setup() {
 	Serial.begin(9600);
@@ -20,32 +31,44 @@ void setup() {
 
 void loop() {
 	//Gyro.Update();
-	//
+	currTime = millis();
 
 	switch(robotState) {
-		case STANDBY: 		Standby();
-		case BEGIN: 		Begin();
-		case LINEFOLLOW: 	LineFollow();
-		case WATERTOWER: 	WaterTower();
-		case CHEMSPILL:	 	ChemSpill();
-		default: 			Standby();
+		case STANDBY: 		Standby(); break;
+		case BEGIN: 		Begin(); break;
+		case LINEFOLLOW: 	LineFollow(); break;
+		case WATERTOWER: 	WaterTower(); break;
+		case CHEMSPILL:	 	ChemSpill(); break;
+		default: 			Standby(); break;
 	}
 }
 
 void Standby() {
-
+	MotorLeft.Set(0);
+	MotorRight.Set(0);
+	MotorLift.Set(0);
+	MotorGrab.Set(0);
+	MotorRotate.Set(0);
 }
 
 void Begin() {
-
 }
 
 void LineFollow() {
-
 }
 
 void WaterTower() {
+	if (WaterTowerState == START) {
+		initalTime = millis();
+	}
 
+	// Note: conditions must be in order of last to first.
+	switch(initalTime - currTime) {
+		case > 100: 
+			MotorLeft.Set(0.0);
+			MotorRight.Set(0.0);
+		case > 50:
+	}
 }
 
 void ChemSpill() {
